940c883913b6b521f6abd0e9bf91ca3de016c118
Stavros Gogos <sgogos1@gmail.com>
2021-06-05 14:18:07 -0700

    [ADDED] Dynamic background image adjustment in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,69 +8,76 @@ window.onload = function(){
     pageSize();
 
     featuredListen.onclick = musicPlayerExpanded;
+    
     for (let i = 0; i < musicArrows.length; i++){
-    musicImages[i].onclick = musicPlayerExpanded;
-    musicArrows[i].onclick = musicPlayerExpanded;
+        musicImages[i].onclick = musicPlayerExpanded;
+        musicArrows[i].onclick = musicPlayerExpanded;
     }
 
     for (let i = 0; i < videoButtons.length; i++){
-    videoButtons[i].onclick = switchVideo;
+        videoButtons[i].onclick = switchVideo;
     }
 
     loadingFinished();
 }
 
 async function loadingFinished(){
+
     for (let i = 0; i < 2; i++){
-    document.getElementById("loading-logo").style.setProperty("filter", 'var(--light-blue-filter)');
-    await new Promise(r => setTimeout(r, 250));
-    document.getElementById("loading-logo").style.setProperty("filter", 'var(--white-filter)');
-    await new Promise(r => setTimeout(r, 250));
+        document.getElementById("loading-logo").style.setProperty("filter", 'var(--light-blue-filter)');
+        await new Promise(r => setTimeout(r, 250));
+        document.getElementById("loading-logo").style.setProperty("filter", 'var(--white-filter)');
+        await new Promise(r => setTimeout(r, 250));
     }
+    
     body.classList.remove("pre-load");
     document.getElementById("loading-wrapper").classList.add("loaded-wrapper");
 }
 
 function pageSize(){
+    body.setAttribute("background-size", `${window.innerWidth} ${window.innerHeight}`);
+    
     let windowLoc;
+    
     if (window.scrollY > Number(header.getAttribute("height"))){
-    body.classList.add("scrolled");
-    body.classList.remove("top");
-    windowLoc = "scrolled";
-    if (!navMenu.classList.contains("nav-open")){
-        logo.classList.remove("visible");
-        logo.classList.add("hidden");
-    }
+        body.classList.add("scrolled");
+        body.classList.remove("top");
+        windowLoc = "scrolled";
+
+        if (!navMenu.classList.contains("nav-open")){
+            logo.classList.remove("visible");
+            logo.classList.add("hidden");
+        }
     }
     else{
-    body.classList.add("top");
-    body.classList.remove("scrolled");
-    windowLoc = "top";
-    if (!navMenu.classList.contains("nav-open")){
-        logo.classList.add("visible");
-        logo.classList.remove("hidden");
-    }
+        body.classList.add("top");
+        body.classList.remove("scrolled");
+        windowLoc = "top";
+        if (!navMenu.classList.contains("nav-open")){
+            logo.classList.add("visible");
+            logo.classList.remove("hidden");
+        }
     }
     if (window.innerWidth <= window.innerHeight){
-    body.classList.add("mobile");
-    body.classList.remove("desktop");
-    adjustScrollClasses(windowLoc, "mobile");
+        body.classList.add("mobile");
+        body.classList.remove("desktop");
+        adjustScrollClasses(windowLoc, "mobile");
     }
     else {
-    body.classList.remove("mobile");
-    body.classList.add("desktop");
-    adjustScrollClasses(windowLoc, "desktop");
+        body.classList.remove("mobile");
+        body.classList.add("desktop");
+        adjustScrollClasses(windowLoc, "desktop");
     }
 }
 
 function adjustScrollClasses(windowLoc, mode){
     if (windowLoc == "top"){
-    body.classList.add(`top-${mode}`);
-    body.classList.remove(`scrolled-${mode}`);
+        body.classList.add(`top-${mode}`);
+        body.classList.remove(`scrolled-${mode}`);
     }
     else {
-    body.classList.add(`scrolled-${mode}`);
-    body.classList.remove(`top-${mode}`);
+        body.classList.add(`scrolled-${mode}`);
+        body.classList.remove(`top-${mode}`);
     }
 
     mode === "mobile" ? mode = "desktop" : mode = "mobile";
@@ -91,12 +98,12 @@ const navMenuLinks = [document.getElementById("music-nav"), document.getElementB
 navButton.onclick = function(){
     if (!navButton.classList.contains("open")){
         if (window.scrollY > Number(header.getAttribute("height")))
-        body.classList.add("nav-open-scrolled")
-        navButton.classList.add("open");
-        navMenu.classList.add("nav-open");
-        navMenu.classList.remove("nav-hidden");
-        logo.classList.add("visible");
-        logo.classList.remove("hidden");
+            body.classList.add("nav-open-scrolled")
+            navButton.classList.add("open");
+            navMenu.classList.add("nav-open");
+            navMenu.classList.remove("nav-hidden");
+            logo.classList.add("visible");
+            logo.classList.remove("hidden");
         for(let i = 0; i < navElements.length; i++) {
             navElements[i].classList.add('visible');
             navElements[i].classList.remove('hidden');
@@ -116,6 +123,7 @@ function closeNavMenu(){
     navButton.classList.remove("open");
     navMenu.classList.add("nav-hidden");
     navMenu.classList.remove("nav-open");
+
     for(let i = 0; i < navElements.length; i++) {
         navElements[i].classList.add('hidden');
         navElements[i].classList.remove('visible');
@@ -142,21 +150,21 @@ function musicPlayerExpanded(event){
     let musicPlayer;
 
     if (arrowClicked === "featured-listen-button"){
-    arrowClicked = "planes-arrow";
+        arrowClicked = "planes-arrow";
     }
     else if (!arrowClicked.includes('arrow')){
-    arrowClicked = arrowClicked.split("-")[0] + "-arrow";
+        arrowClicked = arrowClicked.split("-")[0] + "-arrow";
     }
     
     musicPlayer = document.getElementById(`${arrowClicked.split("-")[0]}-music`);
 
     if (musicPlayer.getAttribute("height") === "0"){
-    musicPlayer.setAttribute("height", "80px");
-    document.getElementById(arrowClicked).classList.add("music-expanded");
+        musicPlayer.setAttribute("height", "80px");
+        document.getElementById(arrowClicked).classList.add("music-expanded");
     }
     else{
-    musicPlayer.setAttribute("height", "0");
-    document.getElementById(arrowClicked).classList.remove("music-expanded");
+        musicPlayer.setAttribute("height", "0");
+        document.getElementById(arrowClicked).classList.remove("music-expanded");
     }
 }
 
@@ -167,7 +175,7 @@ const featuredWatchButton = document.getElementById("featured-watch-button");
 function switchVideo(event){
 
     for (let i = 0; i < videoButtons.length; i++){
-    videoButtons[i].classList.remove("current-video-button");
+        videoButtons[i].classList.remove("current-video-button");
     }
 
     let selectedButton;
@@ -179,14 +187,14 @@ function switchVideo(event){
     const selectedVideo = document.getElementById(selectedButton.id.split("-button")[0]);
 
     if (previousVideo.id !== selectedVideo.id){
-    previousVideo.classList.remove("current-video");
-    previousVideo.classList.add("hidden-video");
-    selectedVideo.classList.add("current-video");
-    selectedVideo.classList.remove("hidden-video");
+        previousVideo.classList.remove("current-video");
+        previousVideo.classList.add("hidden-video");
+        selectedVideo.classList.add("current-video");
+        selectedVideo.classList.remove("hidden-video");
     }
 
     if (event.target.id === 'featured-watch-button'){
-    selectedVideo.setAttribute("src", selectedVideo.getAttribute("src")+"?autoplay=1");
+        selectedVideo.setAttribute("src", selectedVideo.getAttribute("src")+"?autoplay=1");
     }
 }
 
