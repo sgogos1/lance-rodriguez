cafd49204599f488ed04911bbddcff57afdff1f0
Stavros Gogos <sgogos1@gmail.com>
2022-02-18 20:40:01 -0800

    Fixed video switching bug.

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -9,18 +9,16 @@ function generateVisuals(videosJson){
             const id = video.id
             
             let currentVideo = ""
-            if (i == 0){currentVideo = " current-video"}
+            if (video.featured === "True"){currentVideo = " current-video"}
             videoHtml += `<iframe id="${id}" class="video${currentVideo}" src="${video.source}" title="${video.title}" style="border: none" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 
             let currentButton = ""
-            if (i == 0){currentButton = " current-video-button"}
+            if (video.featured === "True"){currentButton = " current-video-button"}
             buttonHtml += `<div id="${id}_button" class="video-nav-circle${currentButton}" title="${video.title}"></div>`
 
             clickableElements.push(`${id}_button`);
 
-            if (video.featured === "True"){
-                featuredVideo = id;
-            }
+            if (video.featured === "True"){featuredVideo = id;}
         }
     }
     return { videoHtml, buttonHtml, clickableElements, featuredVideo};
